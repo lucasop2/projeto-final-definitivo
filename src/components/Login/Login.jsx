e6ecfbd1db24aeccd142768c9a126a19ed1dba7b
MatheusOBarbosa <d2021029612@unifei.edu.br>
2023-07-01 20:01:54 -0300

    mudanças definitivas

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../Login/Style.css";
-import backgroundImage from "../../assets/Leonardo_Diffusion_A_realist_photo_for_a_electric_power_grid_a_1.jpg"
+import backgroundImage from "../../assets/Leonardo_Diffusion_A_realist_photo_for_a_electric_power_grid_a_1.jpg";
 // import imgEdit from "../img/edit.ico";
 
 export default function Usuarios() {
@@ -9,7 +9,6 @@ export default function Usuarios() {
   const [usuarios, setUsuarios] = useState([]);
   const [id, setId] = useState("");
   const [nome, setNome] = useState("");
-  const [sobrenome, setSobrenome] = useState("");
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const [tipo, setTipo] = useState("");
@@ -29,18 +28,16 @@ export default function Usuarios() {
   function limparDados() {
     setId("");
     setNome("");
-    setSobrenome("");
     setEmail("");
     setSenha("");
     setTipo("");
   }
   function editarDados(cod) {
     let usuario = usuarios.find((item) => item.id === cod);
-    const { id, nome, email, sobrenome, senha } = usuario;
+    const { id, nome, email, senha } = usuario;
     setTipo("editar");
     setId(id);
     setNome(nome);
-    setSobrenome(sobrenome);
     setEmail(email);
     setSenha(senha);
   }
@@ -51,7 +48,6 @@ export default function Usuarios() {
     const index = usuarios.findIndex((item) => item.id === id); // 7.3: pode ser alterado
     let users = usuarios;
     users[index].nome = nome;
-    users[index].sobrenome = sobrenome;
     users[index].email = email;
     users[index].senha = senha;
     setUsuarios(users);
@@ -59,14 +55,13 @@ export default function Usuarios() {
   }
   //sera usado para atualização da lista ao criar uma nova conta
   function atualizaListaComNovoUsuario(response) {
-    let { id, nome, email, sobrenome, senha } = response.data[0];
+    let { id, nome, email, senha } = response.data[0];
     let obj = {
       id: id,
       nome: nome,
       email: email,
-      sobrenome: sobrenome,
       senha: senha,
-    }; 
+    };
     let users = usuarios;
     users.push(obj);
     setUsuarios(users);
@@ -74,14 +69,13 @@ export default function Usuarios() {
   }
   //sera usada quando clicar em criar conta ou editar informações
   function gravarDados() {
-    if (nome !== "" && sobrenome !== "" && email !== "" && senha !== "") {
+    if (nome !== "" && email !== "" && senha !== "") {
       if (tipo === "novo") {
         axios
           .post(url + "usuarios", {
             nome: nome,
-            sobrenome: sobrenome, 
             email: email,
-            senha: senha, 
+            senha: senha,
           })
 
           .then((response) => atualizaListaComNovoUsuario(response))
@@ -91,7 +85,6 @@ export default function Usuarios() {
           .put(url + "usuarios/" + id, {
             id: id,
             nome: nome,
-            sobrenome: sobrenome, 
             email: email,
             senha: senha,
           })
@@ -104,26 +97,34 @@ export default function Usuarios() {
     }
   }
 
-  //Call Section: sera usado ao clicar no botao para aparecer a login-form & switch-case
+  //Call Functions: chamada de elementos como login-page, switch-case & show-users
   const [hidden, setHidden] = useState(true);
   const [showSwitchCase, setShowSwitchCase] = useState(false);
+  const [showCadastroLogin, setShowCadastroLogin] = useState(false);
+  const [showUsers, setShowUsers] = useState(false);
+
   const toggleHidden = () => {
     setHidden(!hidden);
     setShowSwitchCase(false); // Reseta o estado do showSwitchCase ao esconder a section
+    setShowCadastroLogin(false);  // Reseta o estado do showCadastroLogin ao esconder a section
   };
   const toggleSwitchCase = () => {
     setShowSwitchCase(!showSwitchCase);
+    setShowCadastroLogin(!showCadastroLogin);
+  };
+  const toggleUsers = () => {
+    setShowUsers(!showUsers); // Função GET para mostrar usuarios do BD
   };
 
   return (
     <div>
-      <section id="btn-call">
+      <section id="btn-call-registro">
         <button type="submit" onClick={toggleHidden}>
           Entrar/Cadastrar
         </button>
       </section>
 
-      <section id="login-form" className={hidden ? "hidden" : ""}>
+      <section id="login-page" className={`${hidden ? "hidden" : ""}`}>
         <div className="img-fundo">
           <h1>
             Seus dados, sua conta: tenha tudo armazenado em um único lugar!
@@ -131,140 +132,140 @@ export default function Usuarios() {
           <img src={backgroundImage} alt="Imagem de fundo" />
         </div>
 
-        <div className="cadastro">
-          <div className="cadastro-registro">
-            <h1>CRIE UMA CONTA</h1>
+        <div className="cadastro-registro">
+          <h1>CRIE UMA CONTA</h1>
 
-            <div className="cadastro-input-1">
-              <p>Nome:</p>
-              <input
-                type="text"
-                name="txtNome"
-                placeholder="nome"
-                value={nome}
-                onChange={(e) => {
-                  setNome(e.target.value);
-                }}
-              />
-            </div>
-
-            <div className="cadastro-input-2">
-              <p>Sobrenome:</p>
-              <input
-                type="text"
-                name="txtSobrenome"
-                placeholder="sobrenome"
-                value={sobrenome}
-                onChange={(e) => {
-                  setSobrenome(e.target.value);
-                }}
-              />
-            </div>
-
-            <div className="cadastro-input-3">
-              <p>Email:</p>
-              <input
-                type="text"
-                name="txtEmail"
-                placeholder="email@exemplo"
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-              />
-            </div>
-
-            <div className="cadastro-input-4">
-              <p>Senha:</p>
-              <input
-                type="text"
-                name="txtSenha"
-                placeholder="senha"
-                value={senha}
-                onChange={(e) => {
-                  setSenha(e.target.value);
-                }}
-              />
-            </div>
-
-            <button type="button" onClick={novosDados}>
-              Novo Perfil
-            </button>
-            <button type="button" onClick={limparDados}>
-              Cancelar
-            </button>
-            <button type="button" onClick={gravarDados}>
-              Gravar
-            </button>
-
-            <div className="switch">
-              <p className="switch-question">Já possui uma conta?</p>
-              <button
-                type="submit"
-                className={showSwitchCase ? "switch-case show" : "switch-case"}
-                onClick={toggleSwitchCase}
-              >
-                LOGIN
-              </button>
-            </div>
+          <div className="cadastro-input-1">
+            <p>Nome de Usuario:</p>
+            <input
+              type="text"
+              name="txtNome"
+              placeholder="username"
+              value={nome}
+              onChange={(e) => {
+                setNome(e.target.value);
+              }}
+            />
           </div>
 
-          {/* <div id="cadastro-login" className="hidden">
+          <div className="cadastro-input-3">
+            <p>Email:</p>
+            <input
+              type="text"
+              name="txtEmail"
+              placeholder="email@exemple"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+
+          <div className="cadastro-input-4">
+            <p>Senha:</p>
+            <input
+              type="text"
+              name="txtSenha"
+              placeholder="password"
+              value={senha}
+              onChange={(e) => {
+                setSenha(e.target.value);
+              }}
+            />
+          </div>
+          
+          <div className="controle-registro">
+            <button type="button" onClick={novosDados}>Novo Perfil</button>
+            <button type="button" onClick={gravarDados}>Gravar</button>
+            <button type="button" onClick={limparDados}>Cancelar</button>
+          </div>
+
+          <p className="switch-question">Já possui uma conta?</p>
+        </div>
+
+        <div id="cadastro-login" className={`${showCadastroLogin ? "" : "hidden"}`}>
+          <div className="cadastro-login-conteudo">  
             <h1>ENTRE EM SUA CONTA</h1>
             <div className="cadastro-input-3">
               <p>Email:</p>
               <input
                 type="text"
                 name="txtEmail"
-                placeholder="Email"
+                placeholder="email@exemple"
                 value={email}
                 onChange={(e) => {
                   setEmail(e.target.value);
                 }}
               />
             </div>
-
-            /* <div className="cadastro-input-4">
+            {" "}
+            <div className="cadastro-input-4">
               <p>Senha:</p>
               <input
                 type="text"
                 name="txtSenha"
-                placeholder="Senha"
+                placeholder="password"
                 value={senha}
                 onChange={(e) => {
                   setSenha(e.target.value);
                 }}
               />
-            </div> */}
+            </div>
+            <div className="controle-login">
+              <button type="button" onClick={novosDados}>Novo Perfil (logar)</button>
+              <button type="button" onClick={limparDados}>Cancelar</button>
+            </div>
+
+            <p className="switch-question">Quer criar uma conta?</p>
+          </div>
         </div>
 
-        {/* <div className="login-close">
-          <button type="submit">X</button>
-        </div> */}
+        <div className="switch">
+            <button
+              type="submit"
+              className={showSwitchCase ? "switch-case show" : "switch-case"}
+              onClick={toggleSwitchCase}
+            >
+              LOGIN
+            </button>
+          </div>
+        
+        <div className="login-close">
+          <button type="submit" onClick={toggleHidden}>
+            X
+          </button>
+        </div>
       </section>
 
-      {usuarios
-        ? usuarios.map((item) => {
-            return (
-              <div key={item.id}>
-                <div>
-                  {" "}
-                  {item.id}-{item.nome}-{item.sobrenome}-{item.email}-
-                  {item.senha}{" "}
-                  {/* #edited: {item.id}-{item.nome}-{item.email}{" "} */}
-                  <img
-                    alt="Editar"
-                    // src={imgEdit}
-                    id={item.id}
-                    height={20}
-                    width={20}
-                    onClick={(e) => editarDados(item.id)}
-                  />
+      {/* Esta seção está relacionada com a chamada dos usuarios do BD */}
+      <section id="btn-call-usuarios">
+        <button type="submit" onClick={toggleUsers}>
+          Users
+        </button>
+      </section>  
+
+      <section id="show-users" className={`${showUsers ? "" : "hidden"}`}>
+        {usuarios
+          ? usuarios.map((item) => {
+              return (
+                <div key={item.id}>
+                  <div>
+                    {" "}
+                    {item.id}-{item.nome}-{item.email}-{item.senha}{" "}
+                    <img
+                      alt="Editar"
+                      // src={imgEdit}
+                      id={item.id}
+                      height={20}
+                      width={20}
+                      onClick={(e) => editarDados(item.id)}
+                    />
+                  </div>
                 </div>
-              </div>
-            );
-          })
-        : false}
+              );
+            })
+          : false}
+      </section>
     </div>
   );
 }
