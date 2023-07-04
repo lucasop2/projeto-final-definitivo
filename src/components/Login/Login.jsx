8cbd812b298ecff1a406106597995f7e1b139d8c
MatheusOBarbosa <84642845+MatheusOBarbosa@users.noreply.github.com>
2023-07-04 01:34:04 -0300

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,16 +2,19 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../Login/Style.css";
 import backgroundImage from "../../assets/Leonardo_Diffusion_A_realist_photo_for_a_electric_power_grid_a_1.jpg";
+import backgroundImage2 from "../../assets/Leonardo_Diffusion_A_detailed_and_stylized_representation_of_a_0.jpg";
 // import imgEdit from "../img/edit.ico";
 
 export default function Usuarios() {
   // variaveis definidas para a interação com o banco de dados
+  const [usuarioLogado, setUsuarioLogado] = useState("");
   const [usuarios, setUsuarios] = useState([]);
   const [id, setId] = useState("");
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const [tipo, setTipo] = useState("");
+  
   const url = "https://apibarbe-lucasop2.vercel.app/";
 
   useEffect(() => {
@@ -32,7 +35,6 @@ export default function Usuarios() {
     setNome("");
     setEmail("");
     setSenha("");
-    setTipo("");
   }
   function editarDados(cod) {
     let usuario = usuarios.find((item) => item.id === cod);
@@ -49,8 +51,8 @@ export default function Usuarios() {
   //sera usado para atualização da lista pos edição
   function atualizaListaUsuarioEditado(response) {
     let { id } = response.data;
-    const index = usuarios.findIndex((item) => item.id === id); // 7.3: pode ser alterado
-    let users = usuarios;
+    const index = usuarios.findIndex((item) => item.id === id);
+    let users = [...usuarios]; // Fazer uma cópia do array para evitar mutação direta
     users[index].nome = nome;
     users[index].email = email;
     users[index].senha = senha;
@@ -68,7 +70,7 @@ export default function Usuarios() {
       email: email,
       senha: senha,
     };
-    let users = usuarios;
+    let users = [...usuarios];
     users.push(obj);
     setUsuarios(users);
     limparDados("");
@@ -99,10 +101,12 @@ export default function Usuarios() {
             senha: senha,
           })
           .then((response) => {
-              atualizaListaUsuarioEditado(response);
-              console.log(response); //conferindo codigo
+            atualizaListaUsuarioEditado(response);
+            console.log(response); //conferindo codigo
           })
           .catch((err) => console.log(err));
+      } else if (tipo === "entrar") {
+        authUser();
       }
     } else {
       console.log("Preencha os campos");
@@ -113,7 +117,10 @@ export default function Usuarios() {
   const [hidden, setHidden] = useState(true);
   const [showSwitchCase, setShowSwitchCase] = useState(false);
   const [showCadastroLogin, setShowCadastroLogin] = useState(false);
+  const [showUserProfile, setshowUserProfile] = useState(false);
   const [showUsers, setShowUsers] = useState(false);
+  const [userFound, setUserFound] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
 
   const toggleHidden = () => {
     console.log("toggleHidden foi chamada");
@@ -127,19 +134,49 @@ export default function Usuarios() {
 
     setShowSwitchCase(!showSwitchCase);
     setShowCadastroLogin(!showCadastroLogin);
+    setIsLogin(!isLogin);
   };
   const toggleUsers = () => {
     console.log("toggleUsers foi chamada");
 
     setShowUsers(!showUsers); // Função GET para mostrar usuarios do BD
   };
+  const toggleProfile = () => {
+    console.log("toggleProfile foi chamada");
+
+    setshowUserProfile(!showUserProfile);  // Reseta o estado do setshowUserProfile ao esconder a section
+  }
+
+  //funções de usuario
+  function authUser() {
+    const usuario = usuarios.find((item) => item.email === email && item.senha === senha);
+    if (usuario) {
+      setUserFound(true);
+      console.log("Usuário encontrado:", usuario);
+      setUsuarioLogado(usuario.nome);
+    } else {
+      setUserFound(false);
+      console.log("Usuário não encontrado");
+      alert("Usuario não encontrado");
+    }
+  }  
 
   return (
     <div>
       <section id="btn-call-registro">
-        <button type="submit" onClick={toggleHidden}>
-          Entrar/Cadastrar
-        </button>
+        {usuarioLogado ? (
+          <a href="#user-page"onClick={toggleProfile}>{usuarioLogado}</a>
+        ) : (
+          <button
+            type="submit"
+            onClick={(e) => {
+              toggleHidden();
+              novosDados();
+            }}
+          >
+            Entrar/Cadastrar
+          </button>
+        )}
       </section>
 
       <section id="login-page" className={`${hidden ? "hidden" : ""}`}>
@@ -191,18 +228,28 @@ export default function Usuarios() {
               }}
             />
           </div>
-          
+
           <div className="controle-registro">
-            <button type="button" onClick={novosDados}>Novo Perfil</button>
-            <button type="button" onClick={gravarDados}>Gravar</button>
-            <button type="button" onClick={limparDados}>Cancelar</button>
+            <button
+              type="button"
+              onClick={(e) => {
+                gravarDados();
+                toggleHidden();
+                alert("Parabens! Sua conta foi registrada com sucesso, agora recarregue a pagina e entre em sua conta em SIGN UP");
+              }}
+            >
+              Continue
+            </button>
+            <button type="button" onClick={limparDados}>
+              Cancelar
+            </button>
           </div>
 
           <p className="switch-question">Já possui uma conta?</p>
         </div>
 
         <div id="cadastro-login" className={`${showCadastroLogin ? "" : "hidden"}`}>
-          <div className="cadastro-login-conteudo">  
+          <div className="cadastro-login-conteudo">
             <h1>ENTRE EM SUA CONTA</h1>
             <div className="cadastro-input-3">
               <p>Email:</p>
@@ -210,13 +257,12 @@ export default function Usuarios() {
                 type="text"
                 name="txtEmail"
                 placeholder="email@exemple"
-                value={email}
+                value={email} // Verifica o tipo para decidir qual valor exibir
                 onChange={(e) => {
                   setEmail(e.target.value);
                 }}
               />
             </div>
-            {" "}
             <div className="cadastro-input-4">
               <p>Senha:</p>
               <input
@@ -230,8 +276,18 @@ export default function Usuarios() {
               />
             </div>
             <div className="controle-login">
-              <button type="button" onClick={novosDados}>Novo Perfil (logar)</button>
-              <button type="button" onClick={limparDados}>Cancelar</button>
+              <button
+                type="button"
+                onClick={(e) => {
+                  authUser();
+                  toggleHidden();
+                }}
+              >
+                Entrar
+              </button>
+              <button type="button" onClick={limparDados}>
+                Cancelar
+              </button>
             </div>
 
             <p className="switch-question">Quer criar uma conta?</p>
@@ -239,19 +295,99 @@ export default function Usuarios() {
         </div>
 
         <div className="switch">
-            <button
-              type="submit"
-              className={showSwitchCase ? "switch-case show" : "switch-case"}
-              onClick={toggleSwitchCase}
-            >
-              LOGIN
-            </button>
-          </div>
-        
+          <button
+            type="submit"
+            className={showSwitchCase ? "switch-case show" : "switch-case"}
+            onClick={toggleSwitchCase}
+          >
+            {isLogin ? "LOGIN" : "SIGN UP"}
+          </button>
+        </div>
+
         <div className="login-close">
           <button type="submit" onClick={toggleHidden}>
             X
           </button>
+        </div>
+      </section>
+
+      <section id="user-page" className={`${showUserProfile ? "" : "hidden"}`}>
+      <div className="img-fundo">
+          <h1>
+          Seu espaço pessoal: organize suas informações em um único local!
+          </h1>
+          <img src={backgroundImage2} alt="Imagem de fundo" />
+        </div>
+
+        <div className="dados-usuario">
+          <h1>Edite suas informações</h1>
+
+          <div className="usuario-dados-nome">
+            <p>Nome de Usuario:</p>
+            <input
+              type="text"
+              name="txtNome"
+              placeholder="username"
+              value={nome}
+              onChange={(e) => {
+                setNome(e.target.value);
+              }}
+            />
+          </div>
+
+          <div className="usuario-dados-email">
+            <p>Email:</p>
+            <input
+              type="text"
+              name="txtEmail"
+              placeholder="email@exemple"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+
+          <div className="usuario-dados-senha">
+            <p>Senha:</p>
+            <input
+              type="text"
+              name="txtSenha"
+              placeholder="password"
+              value={senha}
+              onChange={(e) => {
+                setSenha(e.target.value);
+              }}
+            />
+          </div>
+
+          <div className="controle-dados-usuario">
+            <button
+              type="button"
+              onClick={(e) => {
+                gravarDados();
+                toggleProfile();
+                alert("Para salvar as alterações, recarregue a pagina");
+              }}
+            >
+              Salvar Alterações
+            </button>
+            <button
+              type="button"
+              onClick={(e) => {
+                setUsuarioLogado("");
+                toggleProfile();
+              }}
+            >
+              Deslogar
+            </button>
+          </div>
+
+          <div className="login-close">
+          <button type="submit" onClick={toggleProfile}>
+            X
+          </button>
+        </div>
         </div>
       </section>
 
@@ -260,7 +396,7 @@ export default function Usuarios() {
         <button type="submit" onClick={toggleUsers}>
           Users
         </button>
-      </section>  
+      </section>
 
       <section id="show-users" className={`${showUsers ? "" : "hidden"}`}>
         {usuarios
@@ -270,13 +406,17 @@ export default function Usuarios() {
                   <div>
                     {" "}
                     {item.id}-{item.nome}-{item.email}-{item.senha}{" "}
+                    {usuarioLogado === item.nome && <span>(Usuário Logado)</span>}
                     <img
                       alt="Editar"
                       // src={imgEdit}
                       id={item.id}
                       height={20}
                       width={20}
-                      onClick={(e) => editarDados(item.id)}
+                      onClick={(e) => {
+                        editarDados(item.id);
+                        toggleHidden();
+                      }}
                     />
                   </div>
                 </div>
