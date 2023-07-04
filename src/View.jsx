d13428554e59bd5107f9320617c8d91ecc61d346
MatheusOBarbosa <84642845+MatheusOBarbosa@users.noreply.github.com>
2023-07-04 01:32:45 -0300

    Update View.jsx

--- src/View.jsx
+++ src/View.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./Style.css"
 import Login from "./components/Login/Login"
-// import Navbar from "./components/NavBar/Navbar"
-// import Calculadora from "./components/Calculadora/Calculadora"
+import Navbar from "./components/NavBar/Navbar"
+// import Calculadora from "./components/Principal/Calculadora"
 
 export default function View() {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <Login />
       {/* <Calculadora /> */}
     </>
